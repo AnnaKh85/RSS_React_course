6faf0069d1cbb91661ed52c6015232d44d7814e0
Anna Kh <anna.khizhnyakova@gmail.com>
2024-07-05 07:32:39 +0300

    feat: add components - ErrorBoundary.tsx, Fallback.tsx

---
+++ class-components/src/components/errBoundary/Fallback.tsx
@@ -0,0 +1,18 @@
+function Fallback() {
+    return (
+        <div className="fallback-container">
+            <p>Oops, an error occur...</p>
+            <p>Run away from an error!</p>
+            {/*<button*/}
+            {/*    className="button"*/}
+            {/*    onClick={() => {*/}
+            {/*        window.location.reload();*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    Reload*/}
+            {/*</button>*/}
+        </div>
+    );
+}
+
+export default Fallback;
