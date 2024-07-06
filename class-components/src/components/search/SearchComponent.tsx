5ac753977386fbda42abb077b57de86d9a62bea9
Anna Kh <anna.khizhnyakova@gmail.com>
2024-07-06 08:27:58 +0300

    fix: fix errors after building the project

--- class-components/src/components/search/SearchComponent.tsx
+++ class-components/src/components/search/SearchComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ChangeEvent } from 'react';
+import { Component, ChangeEvent } from 'react';
 
 interface SearchComponentProps {
     onSearch: (searchTerm: string) => void;
