765bb21dc6c39e0c9b1935e72908195324309f25
Kh <anna.khizhnyakova@gmail.com>
2024-08-02 22:45:49 +0300

    Attempt to use Page API. v1.0

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {ThemeProvider} from "./lib/context/ThemeContext";
+
+export const metadata = {
+  title: 'Next.js. Проба в1.0',
+  description: '',
+}
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <ThemeProvider>
+          <body>{children}</body>
+      </ThemeProvider>
+    </html>
+  )
+}
+
